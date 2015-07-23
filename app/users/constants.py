aa63fd1c5b749c65f6e155d5958fe63e5aa39510
kilerd <blove694@gmail.com>
2015-07-23 15:14:42 +0800

    build some constants for user module

---
+++ app/users/constants.py
@@ -0,0 +1,23 @@
+
+#user role
+ADMIN = 0
+USER = 1
+
+ROLE = {
+	ADMIN : 'admin',
+	USER : 'user'
+}
+
+#user status
+
+INACTIVE = 0
+NEW = 1
+ACTIVE = 2
+VIP = 3
+
+STATUS = {
+	INACTIVE : 'inactive',
+	NEW : 'new',
+	ACTIVE : 'active',
+	VIP : 'vip'
+}
