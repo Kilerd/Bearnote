d67a669e1fcc72ebcde665c05ac2d6379c840f3d
Kilerd Chan <blove694@gmail.com>
2015-10-11 00:50:56 +0800

    when delete note , the comment of this note would be deleted auto

--- app/note/views.py
+++ app/note/views.py
@@ -15,46 +15,67 @@ import mistune
 note_module = Blueprint('note_module',__name__)
 
 
+
+# 新建笔记函数
 @note_module.route('/new',methods=['GET','POST'])
 @require_base_domain
 @require_login
 def note_new_function():
+
+    # 初始化 笔记表单的公开分类
     note_new_form = NoteForm()
     note_new_form.public.choices = [('0',u"私有笔记,不公开")]
     for c in NoteCate.objects(belong = User.objects(email = session['user']['email']).first()):
         note_new_form.public.choices.append((c.abbname,c.name))
-    if note_new_form.validate_on_submit():
-        if note_new_form.public.data != '0':
-            public_status = '1'
-            public_cate = NoteCate.objects(abbname = note_new_form.public.data,belong = User.objects(email = session['user']['email']).first()).first()
+    
+
+    # 数据提交处理
+    if request.method == 'POST':
+        if note_new_form.validate_on_submit():
+
+            # 公开分类处理
+            if note_new_form.public.data != '0':
+                public_status = '1'
+                public_cate = NoteCate.objects(abbname = note_new_form.public.data,belong = User.objects(email = session['user']['email']).first()).first()
+            else:
+                public_status = '0'
+                public_cate = None
+
+            # 保存笔记
+            Note(
+                noteid=getnextseq(),
+                title=note_new_form.title.data,
+                subtitle=note_new_form.subtitle.data,
+                content = note_new_form.content.data,
+                public_status = public_status,
+                public_cate = public_cate,
+                tag = note_new_form.tag.data.split(","),
+                belong = (User.objects(email=session['user']['email']).first())
+                ).save()
+
+            return redirect(url_for('note_module.note_new_function'))
+        
+        # 数据填写不完整
         else:
-            public_status = '0'
-            public_cate = None
-
-        Note(
-            noteid=getnextseq(),
-            title=note_new_form.title.data,
-            subtitle=note_new_form.subtitle.data,
-            content = note_new_form.content.data,
-            public_status = public_status,
-            public_cate = public_cate,
-            tag = note_new_form.tag.data.split(","),
-            belong = (User.objects(email=session['user']['email']).first())
-            ).save()
-
-        return redirect(url_for('note_module.note_new_function'))
+            flash(u"内容填写不完整")
+            return redirect(url_for('note_module.note_new_function'))
     return render_template('note/note_new.html',note_new_form = note_new_form)
 
 
+
+# 我的笔记 页面函数
 @note_module.route('/mynote',methods=['GET'])
 @require_base_domain
 @require_login
 def mynote_function():
-    # Page Control
+
+    # 分页设置
     note_count = Note.objects(belong=User.objects(email=session['user']['email']).first()).count()
     page_count = note_count / NOTECONSTANTS.PER_PAGE_COUNT + 1 if note_count % NOTECONSTANTS.PER_PAGE_COUNT else note_count / NOTECONSTANTS.PER_PAGE_COUNT
     if note_count == 0:
         page_count += 1
+
+    # 读取分页
     now_page = request.args.get('page',1)
     try:
         now_page = int(now_page)
@@ -64,10 +85,15 @@ def mynote_function():
         now_page = now_page
     else:
         return redirect(url_for('note_module.mynote_function',page=page_count))
+
+    # 读取该页笔记
     now_page_note = Note.objects(belong=User.objects(email=session['user']['email']).first()).skip(NOTECONSTANTS.PER_PAGE_COUNT*(now_page-1)).limit(NOTECONSTANTS.PER_PAGE_COUNT)
+    
     return render_template('note/my-note.html',notes = now_page_note,page_info = {'page_count':page_count,'now_page':now_page})
 
 
+
+# 笔记墙 页面函数
 @note_module.route('/note/',methods=['GET'])
 @require_base_domain
 def note_wall_function():
@@ -101,130 +127,152 @@ def note_wall_function():
 
 
 
-
+# 单一笔记 页面函数
 @note_module.route("/note/<int:noteid>",methods=['GET','POST'])
 def one_note_function(noteid):
 
+    # 笔记存在性判断
     if Note.objects(noteid=noteid).count() == 0:
         flash(u"找不到这篇文章，不要乱来了。")
         return redirect(url_for('note_module.note_wall_function'))
-    else:
-        this_note = Note.objects(noteid=noteid).first()
-        renderer = mistune.Renderer(escape=True, hard_wrap=True)
-        # use this renderer instance
-        markdown = mistune.Markdown(renderer=renderer)
 
-        this_note.content = markdown(this_note.content)
-        this_note.belong.email_md5 = common.md5_encrypt(this_note.belong.email)
+    # 读取该笔记
+    this_note = Note.objects(noteid=noteid).first()
+    
+    # MarkDown 渲染笔记正文
+    renderer = mistune.Renderer(escape=True, hard_wrap=True)
+    markdown = mistune.Markdown(renderer=renderer)
+    this_note.content = markdown(this_note.content)
+    
+    # 笔记作者邮件MD5 用于显示头像
+    this_note.belong.email_md5 = common.md5_encrypt(this_note.belong.email)
         
-        # 笔记权限判断
-        if this_note.public_status == NOTECONSTANTS.PRIVATE and session['user']['role'] == USERCONSTANTS.USER:
+    # 笔记权限判断
+    if this_note.public_status == NOTECONSTANTS.PRIVATE and session['user']['role'] == USERCONSTANTS.USER:
             
-            if 'user' in session:
-                if this_note.belong.email != session['user']['email']:
-                    flash(u"无权限查看他人的私有笔记。")
-                    return redirect(url_for('note_module.mynote_function'))
-            else:
-                flash(u"你想查看的笔记为私有笔记，无权限查看。")
-                return redirect(url_for('note_module.note_wall_function'))
+        # 登陆 与 非登陆 跳转页面不同
+        if 'user' in session:
+            if this_note.belong.email != session['user']['email']:
+                flash(u"无权限查看他人的私有笔记。")
+                return redirect(url_for('note_module.mynote_function'))
+        else:
+            flash(u"你想查看的笔记为私有笔记，无权限查看。")
+            return redirect(url_for('note_module.note_wall_function'))
 
+    
+    # 评论功能
+    # 需要添加进session 保存最后一次评论的个人信息
+    comment = CommentForm()
 
-        # 评论功能
-        # 需要添加进session 保存最后一次评论的个人信息
-        comment = CommentForm()
+    
+    # POST 页面
+    if request.method == 'POST':
 
-        # POST 页面
-        if request.method == 'POST':
-
-            # 私有笔记 权限判断
-            if this_note.public_status == NOTECONSTANTS.PRIVATE:
-                flash(u"该笔记为私有笔记，无法评论。")
-                return redirect(url_for('note_module.note_wall_function'))
+        # 私有笔记 权限判断
+        if this_note.public_status == NOTECONSTANTS.PRIVATE:
+            flash(u"该笔记为私有笔记，无法评论。")
+            return redirect(url_for('note_module.note_wall_function'))
             
+        # 提交评论
+        if comment.validate_on_submit():
+            Comment(name = comment.name.data,
+                email = comment.email.data.lower(),
+                domain = comment.domain.data,
+                content = comment.content.data,
+                noteid = noteid,).save()
 
-            if comment.validate_on_submit():
-                Comment(name = comment.name.data,
-                    email = comment.email.data.lower(),
-                    domain = comment.domain.data,
-                    content = comment.content.data,
-                    noteid = noteid,).save()
-
-                return redirect(url_for('note_module.one_note_function',noteid=noteid))
-            else:
-                flash(u"内容填写不完整")
-                return redirect(url_for('note_module.one_note_function',noteid=noteid))
+            return redirect(url_for('note_module.one_note_function',noteid=noteid))
+        else:
+            flash(u"内容填写不完整")
+            return redirect(url_for('note_module.one_note_function',noteid=noteid))
         
 
-        # 读取当前笔记的所有评论
-        all_comment = Comment.objects(noteid = noteid)
+    # 读取当前笔记的所有评论
+    all_comment = Comment.objects(noteid = noteid)
         
-        # 评论Email添加MD5 用于头像显示
-        for one_comment in all_comment:
-            one_comment.email_md5 = common.md5_encrypt(one_comment.email)
+    # 评论Email添加MD5 用于头像显示
+    for one_comment in all_comment:
+        one_comment.email_md5 = common.md5_encrypt(one_comment.email)
 
 
-        return render_template('/note/one_note.html',this_note=this_note, comment=comment, all_comment=all_comment)
+    return render_template('/note/one_note.html',this_note=this_note, comment=comment, all_comment=all_comment)
 
 
+# 笔记编辑
 @note_module.route("/note/edit/<int:noteid>",methods=['GET','POST'])
 @require_base_domain
 @require_login
 def note_edit_function(noteid):
+
+    # 笔记存在性判断
     if Note.objects(noteid=noteid,belong=User.objects(email=session['user']['email']).first()).count() == 0:
         flash(u"找不到这篇文章，不要乱来了。")
         return redirect(url_for('note_module.mynote_function'))
-    else:
-        this_note = Note.objects(noteid=noteid).first()
-        note_edit_form = NoteForm(
-            title = this_note.title,
-            subtitle = this_note.subtitle,
-            content = this_note.content,
-            tag = ",".join(this_note.tag)
-            )
-        note_edit_form.public.choices = [('0',u"私有笔记,不公开")]
-        for c in NoteCate.objects(belong = User.objects(email = session['user']['email']).first()):
-            note_edit_form.public.choices.append((c.abbname,c.name))
 
-        if request.method == 'GET' and this_note.public_status == NOTECONSTANTS.PUBLIC:
-            note_edit_form.public.data = this_note.public_cate.abbname
+    # 读取该笔记
+    this_note = Note.objects(noteid=noteid).first()
 
-        if request.method == 'POST':
-            if note_edit_form.validate_on_submit():
-                this_note.title = note_edit_form.title.data
-                this_note.subtitle = note_edit_form.subtitle.data
-                this_note.subtitle = note_edit_form.subtitle.data
-                this_note.content = note_edit_form.content.data
-                this_note.tag = note_edit_form.tag.data.split(',')
-                print note_edit_form.public.data
-                if note_edit_form.public.data != '0':
-                    print 'this for public'
-                    public_status = '1'
-                    public_cate = NoteCate.objects(abbname = note_edit_form.public.data,belong = User.objects(email = session['user']['email']).first()).first()
-                else:
-                    print 'this for private'
-                    public_status = '0'
-                    public_cate = None
+    # 编辑表单 设置
+    note_edit_form = NoteForm(
+        title = this_note.title,
+        subtitle = this_note.subtitle,
+        content = this_note.content,
+        tag = ",".join(this_note.tag)
+        )
+    # 加载笔记公开分类
+    note_edit_form.public.choices = [('0',u"私有笔记,不公开")]
+    for c in NoteCate.objects(belong = User.objects(email = session['user']['email']).first()):
+        note_edit_form.public.choices.append((c.abbname,c.name))
 
-                print public_status
-                this_note.public_status = public_status
-                this_note.public_cate = public_cate
-                this_note.save()
-                flash(u"笔记修改成功")
-                return redirect(url_for('note_module.one_note_function',noteid=this_note.noteid))
+    # 当GET页面时 显示默认的笔记分类
+    if request.method == 'GET' and this_note.public_status == NOTECONSTANTS.PUBLIC:
+        note_edit_form.public.data = this_note.public_cate.abbname
+
+    # 数据提交检验
+    if request.method == 'POST':
+
+        if note_edit_form.validate_on_submit():
+
+            # 数据提交
+            this_note.title = note_edit_form.title.data
+            this_note.subtitle = note_edit_form.subtitle.data
+            this_note.subtitle = note_edit_form.subtitle.data
+            this_note.content = note_edit_form.content.data
+            this_note.tag = note_edit_form.tag.data.split(',')
+            if note_edit_form.public.data != '0':
+                this_note.public_status = '1'
+                this_note.public_cate = NoteCate.objects(abbname = note_edit_form.public.data,belong = User.objects(email = session['user']['email']).first()).first()
             else:
-                flash(u"笔记内容不完整")
+                this_note.public_status = '0'
+                this_note.public_cate = None
+            this_note.save()
+            flash(u"笔记修改成功")
+            return redirect(url_for('note_module.one_note_function',noteid=this_note.noteid))
+        
+        else:
+            flash(u"笔记内容不完整")
 
-        return render_template('/note/note_edit.html',note_edit_form=note_edit_form,this_note=this_note)
+    return render_template('/note/note_edit.html',note_edit_form=note_edit_form,this_note=this_note)
 
+
+# 笔记删除
 @note_module.route('/note/delete/<int:noteid>',methods=['GET'])
 @require_base_domain
 @require_login
 def note_delete_function(noteid):
+
+    # 笔记存在性判断
     if Note.objects(noteid=noteid,belong=User.objects(email=session['user']['email']).first()).count() == 0:
         flash(u"找不到这篇文章，不要乱来了。")
         return redirect(url_for('note_module.mynote_function'))
+    
+    # 删除笔记
     this_note = Note.objects(noteid=noteid,belong=User.objects(email=session['user']['email']).first()).first()
     Note.delete(this_note)
-    #Delete Commit
+    
+    # 删除笔记下的评论
+    for one_comment in Comment.objects(noteid = noteid):
+        Comment.delete(one_comment)
+    
     flash(u"笔记删除成功")
     return redirect(url_for('note_module.mynote_function'))
